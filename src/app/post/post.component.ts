2a22e125b9a70fcf175b1e2af7d9d7dc3ab029d4
Adrien Rossato <adrien.marcos@gmail.com>
2022-11-20 16:30:47 -0300

    learning lifecycles

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,14 +1,22 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
   @Input('img')
   postImg = '';
 
   @Output()
   imgSelected = new EventEmitter<string>();
+
+  constructor() {
+    console.log('constructor() called', this.postImg);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit() called', this.postImg);
+  }
 }
