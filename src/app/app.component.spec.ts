687e60dd4ed47e805b551e881ef2dfae9dadd0d8
Adrien Rossato <adrien.marcos@gmail.com>
2022-11-20 14:48:47 -0300

    finnishing setting up our application

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-basics'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-basics');
+    expect('title').toEqual('angular-basics');
   });
 
   it('should render title', () => {
