3cb41f04cc3d54ef505c7cf2362928be4c5bc3c4
Adrien Rossato <adrien.marcos@gmail.com>
2022-11-20 16:00:07 -0300

    creating our first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from "@angular/core";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  imgURL = 'https://picsum.photos/id/237/500/500';
 
+  changeImage(e: KeyboardEvent) {
+    this.imgURL = (e.target as HTMLInputElement).value;
+  }
+
+  logImg(event: string) {
+    console.log(event);
+  }
 }
