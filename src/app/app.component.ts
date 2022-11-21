7d05ecc2ae3dabfbd7a26c32d243b7add7bf2654
Adrien Rossato <adrien.marcos@gmail.com>
2022-11-21 10:31:24 -0300

    learning directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,14 @@ import { Component } from "@angular/core";
 export class AppComponent {
   cost = 2000;
   name = 'adrien';
+  blueClass = false;
   temperature = 25.3;
   currentDate = new Date();
   imgURL = 'https://picsum.photos/id/237/500/500';
+  pizza = {
+    toppings: ['pepperoni', 'bacon'],
+    size: 'large'
+  }
 
   changeImage(e: KeyboardEvent) {
     this.imgURL = (e.target as HTMLInputElement).value;
