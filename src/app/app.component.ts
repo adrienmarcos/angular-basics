a0e515ff80894ace4daeb0f3e2d1fd96bff7b79b
Adrien Rossato <adrien.marcos@gmail.com>
2022-11-20 22:02:29 -0300

    learning more pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,10 @@ import { Component } from "@angular/core";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  cost = 2000;
   name = 'adrien';
+  temperature = 25.3;
+  currentDate = new Date();
   imgURL = 'https://picsum.photos/id/237/500/500';
 
   changeImage(e: KeyboardEvent) {
